e3895ec86a11af04107e9fe7cf3aca9f2b9d6c05
Andrew-Nes <cejosoll@gmail.com>
2025-06-09 11:33:41 +0300

    tests: setup testing environment, add setup layout test

---
+++ cards-ad/tests/setup-layout.test.ts
@@ -0,0 +1,70 @@
+import { describe, it, expect, vi, beforeEach } from 'vitest';
+
+import { setupLayout } from 'src/components/layout/setup-layout/setup-layout';
+
+vi.mock('src/components/layout/table/table', () => ({
+  createTable: (root: HTMLElement) => {
+    const p = document.createElement('p');
+    p.textContent = 'table created';
+    root.appendChild(p);
+  }
+}));
+vi.mock('src/components/layout/endgame-overlay/endgame-overlay', () => ({
+  createEndGameOverlay: (root: HTMLElement) => {
+    const p = document.createElement('p');
+    p.textContent = 'overlay created';
+    root.appendChild(p);
+  }
+}));
+vi.mock('src/components/game/init-game/init-game', () => ({
+  initGame: vi.fn(),
+}));
+
+import { initGame } from 'src/components/game/init-game/init-game';
+
+
+describe('setupLayout', () => {
+  let root: HTMLElement;
+
+  beforeEach(() => {
+    document.body.innerHTML = '';
+    root = document.createElement('div');
+    document.body.appendChild(root);
+
+    vi.clearAllMocks();
+  });
+
+  it('adds the main title to the root', () => {
+    setupLayout(root);
+
+    const title = root.querySelector('.main-title') as HTMLElement;
+    expect(title).not.toBeNull();
+    expect(title.textContent).toBe('⚔️ Card Clash ⚔️');
+  });
+
+  it('creates table,', () => {
+    setupLayout(root);
+
+    const p = root.querySelector('p');
+
+    expect(p).toBeTruthy();
+    expect(p?.textContent).toBe('table created');
+
+  });
+
+  it('creates EndGameOverlay', () => {
+    setupLayout(root);
+
+    const p = root.querySelector('p');
+
+    expect(p).toBeTruthy();
+    expect(p?.textContent).toBe('table created');
+
+  });
+
+  it('calls initGame', () => {
+    setupLayout(root);
+
+    expect(initGame).toHaveBeenCalled();
+  });
+});
---
+++ cards-ad/vitest.config.ts
@@ -0,0 +1,13 @@
+import path from 'path';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    alias: {
+
+      'src': path.resolve(__dirname, './src'),
+    },
+  }
+});
