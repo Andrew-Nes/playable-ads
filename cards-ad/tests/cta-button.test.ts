8e9fe20e48c913649e3a56ba1142c0161e0cafa3
Andrew-Nes <cejosoll@gmail.com>
2025-06-11 17:18:22 +0300

    test: add cta-button test

---
+++ cards-ad/tests/cta-button.test.ts
@@ -0,0 +1,31 @@
+import { describe, it, expect, beforeEach } from 'vitest';
+
+import { createCtaButton } from "src/components/layout/cta-button/cta-button";
+
+
+
+describe('Message Window Module', () => {
+  let root: HTMLElement;
+
+  beforeEach(() => {
+    document.body.innerHTML = '';
+    root = document.createElement('div');
+    document.body.appendChild(root);
+
+    const mockLocation = {
+    href: 'https://example.com',
+  };
+
+  vi.stubGlobal('location', mockLocation);
+  });
+
+  it('should create a cta button in the DOM', async () => {
+    createCtaButton(root);
+
+    const buttonEl = root.querySelector('.cta-button');
+
+    expect(buttonEl).toBeTruthy();
+    expect(buttonEl?.textContent).toBe('Continue playing');
+    expect(location.href).toBe('https://example.com'); 
+  }); 
+});
